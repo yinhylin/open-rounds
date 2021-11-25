f8e0f076e90e84fd712c2433d6c1079722228828
sailormoon <sailormoon@users.noreply.github.com>
2021-11-25 00:10:54 -0800

    Order StateBuffer by importance.

--- world/state_buffer.go
+++ world/state_buffer.go
@@ -14,109 +14,12 @@ type StateBuffer struct {
 	currentTick  int64
 }
 
-func (s *StateBuffer) ForEachBullet(callback func(string, *Bullet)) {
-	if current := s.Current(); current != nil {
-		for ID, bullet := range current.Bullets {
-			callback(ID, &bullet)
-		}
-	}
-}
-
-func (s *StateBuffer) ForEachPlayer(callback func(string, *Player)) {
-	if current := s.Current(); current != nil {
-		for ID, entity := range current.Players {
-			callback(ID, &entity)
-		}
-	}
-}
-
-func (s *StateBuffer) CurrentTick() int64 {
-	return s.currentTick
-}
-
-func newRingBuffer(maxCapacity int) []*State {
-	states := make([]*State, maxCapacity)
-	for i := range states {
-		states[i] = NewState()
-	}
-	return states
-}
-
-func (s *StateBuffer) Clear() {
-	s.states = newRingBuffer(cap(s.states))
-	s.currentTick = NilTick
-}
-
 func NewStateBuffer(maxCapacity int) *StateBuffer {
 	return &StateBuffer{
 		states:       newRingBuffer(maxCapacity),
 		futureEvents: make(map[int64][]*pb.ServerEvent),
 		currentTick:  NilTick,
 	}
-}
-
-func (s *StateBuffer) Add(state *State) {
-	index := (s.index + 1) % cap(s.states)
-	if s.states[s.index].Tick == NilTick {
-		index = s.index
-	}
-	s.index = index
-	s.states[index] = state
-	s.currentTick = state.Tick
-}
-
-func (s *StateBuffer) Next() *State {
-	current := s.Current()
-	if current == nil {
-		return nil
-	}
-
-	next := Simulate(current)
-	s.Add(next)
-	for _, event := range s.futureEvents[s.currentTick] {
-		s.OnEvent(event)
-	}
-	delete(s.futureEvents, s.currentTick)
-	return next
-}
-
-func (s *StateBuffer) Current() *State {
-	current := s.states[s.index]
-	if current.Tick == NilTick {
-		return nil
-	}
-	return current
-}
-
-func (s *StateBuffer) walkNextStates(index int, steps int, callback func(int)) {
-	for i := 1; i <= steps; i++ {
-		nextIndex := (index + i) % cap(s.states)
-		if s.states[index].Tick > s.states[nextIndex].Tick {
-			log.Fatal(s.states[i].Tick, s.states[nextIndex].Tick)
-		}
-		callback((index + i) % cap(s.states))
-	}
-}
-
-func (s *StateBuffer) applyUpdate(tick int64, callback func(*State)) error {
-	if s.currentTick < tick {
-		log.Fatal(tick, s.currentTick)
-	}
-
-	for i, state := range s.states {
-		if state.Tick != tick {
-			continue
-		}
-		callback(s.states[i])
-		currentState := s.states[i]
-		// Re-simulate.
-		s.walkNextStates(i, int(s.currentTick-state.Tick), func(index int) {
-			s.states[index] = Simulate(currentState)
-			currentState = s.states[index]
-		})
-		return nil
-	}
-	return fmt.Errorf("could not find tick. current=%d, server=%d", s.currentTick, tick)
 }
 
 func (s *StateBuffer) OnEvent(e *pb.ServerEvent) error {
@@ -172,6 +75,102 @@ func (s *StateBuffer) OnEvent(e *pb.ServerEvent) error {
 		})
 	}
 	return fmt.Errorf("unhandled event=%+v\n", e)
+}
+
+func (s *StateBuffer) Next() *State {
+	current := s.Current()
+	if current == nil {
+		return nil
+	}
+
+	next := Simulate(current)
+	s.Add(next)
+	for _, event := range s.futureEvents[s.currentTick] {
+		s.OnEvent(event)
+	}
+	delete(s.futureEvents, s.currentTick)
+	return next
+}
+
+func (s *StateBuffer) Current() *State {
+	if current := s.states[s.index]; current.Tick != NilTick {
+		return current
+	}
+	return nil
+}
+
+func (s *StateBuffer) Clear() {
+	s.states = newRingBuffer(cap(s.states))
+	s.currentTick = NilTick
+}
+
+func (s *StateBuffer) Add(state *State) {
+	index := (s.index + 1) % cap(s.states)
+	if s.states[s.index].Tick == NilTick {
+		index = s.index
+	}
+	s.index = index
+	s.states[index] = state
+	s.currentTick = state.Tick
+}
+
+func (s *StateBuffer) ForEachBullet(callback func(string, *Bullet)) {
+	if current := s.Current(); current != nil {
+		for ID, bullet := range current.Bullets {
+			callback(ID, &bullet)
+		}
+	}
+}
+
+func (s *StateBuffer) ForEachPlayer(callback func(string, *Player)) {
+	if current := s.Current(); current != nil {
+		for ID, entity := range current.Players {
+			callback(ID, &entity)
+		}
+	}
+}
+
+func (s *StateBuffer) CurrentTick() int64 {
+	return s.currentTick
+}
+
+func newRingBuffer(maxCapacity int) []*State {
+	states := make([]*State, maxCapacity)
+	for i := range states {
+		states[i] = NewState()
+	}
+	return states
+}
+
+func (s *StateBuffer) walkNextStates(index int, steps int, callback func(int)) {
+	for i := 1; i <= steps; i++ {
+		nextIndex := (index + i) % cap(s.states)
+		if s.states[index].Tick > s.states[nextIndex].Tick {
+			log.Fatal(s.states[i].Tick, s.states[nextIndex].Tick)
+		}
+		callback((index + i) % cap(s.states))
+	}
+}
+
+func (s *StateBuffer) applyUpdate(tick int64, callback func(*State)) error {
+	if s.currentTick < tick {
+		log.Fatal(tick, s.currentTick)
+	}
+
+	for i, state := range s.states {
+		if state.Tick != tick {
+			continue
+		}
+		callback(s.states[i])
+		currentState := s.states[i]
+		// Re-simulate.
+		s.walkNextStates(i, int(s.currentTick-state.Tick), func(index int) {
+			s.states[index] = Simulate(currentState)
+			currentState = s.states[index]
+		})
+		return nil
+	}
+	return fmt.Errorf("could not find tick. current=%d, server=%d", s.currentTick, tick)
 }
 
 func StateBufferFromProto(p *pb.StateBuffer) *StateBuffer {
