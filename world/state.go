a63a6609d08552abba0ebc821e9a18b110291bd1
sailormoon <sailormoon@users.noreply.github.com>
2021-11-24 22:42:30 -0800

    Move messages to separate file.

--- world/state.go
+++ world/state.go
@@ -45,40 +45,6 @@ func (s *State) ToProto() *pb.State {
 	}
 }
 
-type IntentsUpdate struct {
-	ID      string
-	Intents map[pb.Intents_Intent]struct{}
-	Tick    int64
-}
-
-type EntityUpdate struct {
-	ID     string
-	Entity Entity
-	Tick   int64
-}
-
-type AngleUpdate struct {
-	ID    string
-	Angle float64
-	Tick  int64
-}
-
-type AddEntity struct {
-	ID   string
-	Tick int64
-}
-
-type AddBullet struct {
-	Source string
-	ID     string
-	Tick   int64
-}
-
-type RemoveEntity struct {
-	ID   string
-	Tick int64
-}
-
 func IntentsEqual(a, b map[pb.Intents_Intent]struct{}) bool {
 	if len(a) != len(b) {
 		return false
