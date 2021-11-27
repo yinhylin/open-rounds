272d810d034ec0b06d496d1225769dcd0cbc99d9
sailormoon <sailormoon@fastmail.com>
2021-11-26 22:29:24 -0800

    Disable lerping because it lags on 144FPS.

--- client/renderer.go
+++ client/renderer.go
@@ -26,39 +26,40 @@ type Renderer struct {
 
 func NewRenderer(lerpFactor float64) *Renderer {
 	return &Renderer{
-		renderData: make(map[string]*RenderData),
-		lerpFactor: lerpFactor,
-	}
+		renderData: make(map[string]*RenderData), lerpFactor: lerpFactor}
 }
 
 func (r *Renderer) RenderPlayer(screen *ebiten.Image, playerImage *ebiten.Image, gunImage *ebiten.Image, p *world.Player) {
 	opt := &ebiten.DrawImageOptions{}
 	drawCoords := p.Coords
-	if renderData, ok := r.renderData[p.ID]; ok {
-		distance := math.Sqrt(math.Pow(renderData.lastPlayerDrawCoords.X-drawCoords.X, 2) + math.Pow(renderData.lastPlayerDrawCoords.Y-drawCoords.Y, 2))
-		lerpFactor := r.lerpFactor
-		if distance < 8 {
-			lerpFactor /= 2
-		}
-		if time.Since(r.lastDrawTime).Seconds() <= r.lerpFactor && distance > 1 {
-			lerpDelta := time.Since(r.lastDrawTime).Seconds() / lerpFactor
-			drawCoords = world.Vector{
-				X: Lerp(renderData.lastPlayerDrawCoords.X, drawCoords.X, lerpDelta),
-				Y: Lerp(renderData.lastPlayerDrawCoords.Y, drawCoords.Y, lerpDelta),
+	/*
+		if renderData, ok := r.renderData[p.ID]; ok {
+			distance := math.Sqrt(math.Pow(renderData.lastPlayerDrawCoords.X-drawCoords.X, 2) + math.Pow(renderData.lastPlayerDrawCoords.Y-drawCoords.Y, 2))
+			lerpFactor := r.lerpFactor
+			if distance < 8 {
+				lerpFactor /= 2
+			}
+			if time.Since(r.lastDrawTime).Seconds() <= r.lerpFactor && distance > 1 {
+				lerpDelta := time.Since(r.lastDrawTime).Seconds() / lerpFactor
+				drawCoords = world.Vector{
+					X: Lerp(renderData.lastPlayerDrawCoords.X, drawCoords.X, lerpDelta),
+					Y: Lerp(renderData.lastPlayerDrawCoords.Y, drawCoords.Y, lerpDelta),
+				}
+			}
+		} else {
+			r.renderData[p.ID] = &RenderData{
+				lastPlayerDrawCoords: drawCoords,
+				prevAngle:            p.Angle,
 			}
 		}
-	} else {
-		r.renderData[p.ID] = &RenderData{
-			lastPlayerDrawCoords: drawCoords,
-			prevAngle:            p.Angle,
-		}
-	}
+	*/
+
 	opt.GeoM.Translate(drawCoords.X, drawCoords.Y)
 	opt.Filter = ebiten.FilterLinear
 	_, playerHeight := playerImage.Size()
-	r.renderData[p.ID].lastPlayerDrawCoords = drawCoords
+	// r.renderData[p.ID].lastPlayerDrawCoords = drawCoords
 	screen.DrawImage(playerImage, opt)
-	RenderGun(screen, gunImage, r.renderData[p.ID].lastPlayerDrawCoords, p.Angle)
+	RenderGun(screen, gunImage, drawCoords, p.Angle)
 	debugString := fmt.Sprintf("%s\n(%0.0f,%0.0f)\n%d :: %d", p.ID, p.Coords.X, p.Coords.Y, p.Health, p.Ammo)
 	ebitenutil.DebugPrintAt(screen, debugString, int(drawCoords.X), int(drawCoords.Y)+playerHeight)
 }
